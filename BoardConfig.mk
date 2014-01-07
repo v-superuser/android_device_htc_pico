fdefba302182062807214a8fb1aa82b74c18b5a9
Vineeth Raj <vineethraj49@gmail.com>
2014-01-07 18:49:15 +0530

    Bring back lumfile for fixing mass storage

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,6 +95,7 @@ BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
 
 BOARD_USE_USB_MASS_STORAGE_SWITCH := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun0/file"
+BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun0/file"
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
